995c8659c5a30c6a52e6c4cb56232753e4a81b67
Jacob Landman <jakeypoolandman7@gmail.com>
2017-02-13 19:36:44 -0600

    Created ui for the calculator view

---
+++ chartofnuclides/RadialGradientView.swift
@@ -0,0 +1,21 @@
+//
+//  RadialGradientView.swift
+//  chartofnuclides
+//
+//  Created by Jacob Landman on 2/13/17.
+//  Copyright © 2017 Jacob Landman. All rights reserved.
+//
+
+import UIKit
+
+class RadialGradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ chartofnuclides/UIViewExtension+AddShadow.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewExtension+AddShadow.swift
+//  chartofnuclides
+//
+//  Created by Jacob Landman on 2/13/17.
+//  Copyright © 2017 Jacob Landman. All rights reserved.
+//
+
+import UIKit
+
+class UIViewExtension_AddShadow: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
