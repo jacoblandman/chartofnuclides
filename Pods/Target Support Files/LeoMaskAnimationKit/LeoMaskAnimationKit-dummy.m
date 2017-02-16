deb9c907122ac6c6b018849e5078c0f031fb9762
Jacob Landman <jakeypoolandman7@gmail.com>
2017-02-15 18:19:05 -0600

    added pod files

---
+++ Pods/Target Support Files/LeoMaskAnimationKit/LeoMaskAnimationKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LeoMaskAnimationKit : NSObject
+@end
+@implementation PodsDummy_LeoMaskAnimationKit
+@end
---
+++ Pods/Target Support Files/LeoMaskAnimationKit/LeoMaskAnimationKit-umbrella.h
@@ -0,0 +1,9 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "UIView+LeoMaskAnimation.h"
+
+FOUNDATION_EXPORT double LeoMaskAnimationKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char LeoMaskAnimationKitVersionString[];
+
