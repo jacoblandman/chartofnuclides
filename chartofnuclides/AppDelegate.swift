1b5c0076164d39aeaaf15c6f4daf7ad76af98b7d
Jacob Landman <jakeypoolandman7@gmail.com>
2017-02-16 16:33:17 -0600

    Added new animations to the tab bar

--- chartofnuclides/AppDelegate.swift
+++ chartofnuclides/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RAMAnimatedTabBarController
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,8 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        UITabBar.appearance().isTranslucent = false
-        UITabBar.appearance().tintColor = UIColor.white
+        if let tabController = window?.rootViewController as? RAMAnimatedTabBarController {
+            tabController.changeSelectedColor(UIColor.white, iconSelectedColor: UIColor.white)
+        }
         
         return true
     }
