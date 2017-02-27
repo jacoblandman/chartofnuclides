e51741b174b5d3fd45511d1ca0613d21abda8e2a
Jacob Landman <jakeypoolandman7@gmail.com>
2017-02-27 12:17:08 -0600

    Added ui for the community login screen

---
+++ chartofnuclides/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  chartofnuclides
+//
+//  Created by Jacob Landman on 2/27/17.
+//  Copyright © 2017 Jacob Landman. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func emailLoginPressed(_ sender: Any) {
+    }
+
+    @IBAction func facebookLoginPressed(_ sender: Any) {
+    }
+}
---
+++ chartofnuclides/ProfileVC.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileVC.swift
+//  chartofnuclides
+//
+//  Created by Jacob Landman on 2/27/17.
+//  Copyright © 2017 Jacob Landman. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
