3385e0206ff563994c510dd1758cc63cc3e1f749
Jacob Landman <jakeypoolandman7@gmail.com>
2017-02-28 17:17:44 -0600

    added retrieval of user data from profile to display on the profile VC

--- chartofnuclides/ProfileVC.swift
+++ chartofnuclides/ProfileVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ProfileVC: UIViewController {
 
@@ -15,10 +16,31 @@ class ProfileVC: UIViewController {
     @IBOutlet weak var commentsLbl: UILabel!
     @IBOutlet weak var questionsLbl: UILabel!
     @IBOutlet weak var repuationLbl: UILabel!
+    @IBOutlet weak var activityIndicatorView: InspectableBorderView!
+    var user: User?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+        if let currentUser = FIRAuth.auth()?.currentUser {
+            activityIndicatorView.isHidden = false
+            user = User(uid: currentUser.uid)
+            user?.loadUserInfo(completed: { 
+                self.updateUI()
+            })
+        } else {
+            print("JACOB: Error current user not found")
+        }
+    }
+    
+    func updateUI() {
+        // we know a user exists at this point
+        self.usernameLbl.text = user!.username
+        self.commentsLbl.text = user!.comments
+        self.questionsLbl.text = user!.questions
+        self.repuationLbl.text = user!.reputation
+        activityIndicatorView.isHidden = true
     }
 
     @IBAction func changeProfileImgPressed(_ sender: Any) {
@@ -37,11 +59,13 @@ class ProfileVC: UIViewController {
     
     @IBAction func deleteAccountPressed(_ sender: Any) {
         // present action controller to verify user wants to log out
+
+        guard let uid = user?.uid, let username = user?.username else { return }
         
         let ac = UIAlertController(title: "Deleting Account", message: "Are you sure about this? There is no going back...", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         ac.addAction(UIAlertAction(title: "Yes, I'm sure", style: .default, handler: { (alert: UIAlertAction!) in
-            AuthService.instance.deleteCurrentUser()
+            AuthService.instance.deleteCurrentUser(uid: uid, username: username)
             self.dismiss(animated: true, completion: nil)
         }))
         present(ac, animated: true, completion: nil)
--- chartofnuclides/User.swift
+++ chartofnuclides/User.swift
@@ -7,39 +7,88 @@
 //
 
 import UIKit
+import Firebase
 
-struct User {
-    private var _username: String
+typealias userDownloadComplete = () -> ()
+
+class User {
     private var _uid: String
-    private var _repuation: String
-    private var _questions: String
-    private var _comments: String
+    private var _username: String!
+    private var _repuation: String!
+    private var _questions: String!
+    private var _comments: String!
     
     var uid: String {
         return _uid
     }
     
     var username: String {
+        if _username == nil {
+            _username = ""
+        }
         return _username
     }
     
     var reputation: String {
+        if _repuation == nil {
+            _repuation = ""
+        }
         return _repuation
     }
     
     var questions: String {
+        if _questions == nil {
+            _questions = ""
+        }
         return _questions
     }
     
     var comments: String {
+        if _comments == nil {
+            _comments = ""
+        }
         return _comments
     }
     
-    init(uid: String, username: String, reputation: String, questions: String, comments: String) {
+    init(uid: String) {
         _uid = uid
-        _username = username
-        _repuation = reputation
-        _questions = questions
-        _comments = comments
+    }
+    
+    func loadUserInfo(completed: @escaping userDownloadComplete) {
+        let userRef = DataService.instance.usersRef.child(self.uid)
+        
+        userRef.observeSingleEvent(of: .value, with: { (snapshot) in
+            
+        let profileSnap = snapshot.childSnapshot(forPath: "profile")
+            print("JACOB: Found profile")
+            if let profileDict = profileSnap.value as? Dictionary<String, AnyObject> {
+                if let questionsAsked = profileDict["questionsAsked"] as? String {
+                    self._questions = questionsAsked
+                }
+                if let comments = profileDict["comments"] as? String {
+                    self._comments = comments
+                }
+                if let reputation = profileDict["reputation"] as? String {
+                    self._repuation = reputation
+                    print("REPUTATION: ", reputation)
+                }
+            }
+            
+        let usernameSnap = snapshot.childSnapshot(forPath: "username")
+            print("JACOB: Found username")
+            if let usernameDict = usernameSnap.value as? Dictionary<String, AnyObject> {
+                if let username = usernameDict["username"] as? String {
+                    self._username = username
+                }
+            }
+        
+        completed()
+            
+        })
+        
+    }
+    
+    deinit {
+        print("JACOB: User object has been deinited")
     }
 }
